84eb4c40911eef065dbd50ea3fe14ddd5954c400
Shohei Yamasaki <shoyamas@yahoo-corp.jp>
2018-03-20 18:02:29 +0900

    first commit

---
+++ src/main/java/UnusedCode.java
@@ -0,0 +1,7 @@
+public class UnusedLocalVariable {
+    private static int FOO = 2; // Unused
+    public void doSomething() {
+        int i = 5; // Unused
+    }
+    private void foo() {} // unused
+}
